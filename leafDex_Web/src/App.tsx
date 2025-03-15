9092e7f1baebf47eae08aa462539c07278063f78
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2025-03-14 21:01:31 -0400

    nav bar done

--- leafDex_Web/src/App.tsx
+++ leafDex_Web/src/App.tsx
@@ -5,11 +5,28 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <>{/* New top navigation */}
+           <nav className="top-nav">
+        <ul className="nav-list">
+          <li>
+            <a href="#home" className="nav-link">Home</a>
+          </li>
+          <li>
+            <a href="#about" className="nav-link">About</a>
+          </li>
+          <li>
+            <a href="#products" className="nav-link">Products</a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-link">Contact</a>
+          </li>
+        </ul> </nav>
+{/* 
+    /*logo */ }
       <div>
         <img src={leafDex} className="logo" alt="Vite logo" />
       </div>
-      <h1>LeafDex</h1>
+      <h1>Leafdex</h1>
     </>
   )
 }
