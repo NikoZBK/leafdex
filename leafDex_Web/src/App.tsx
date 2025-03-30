e94e1f3732f5ddb31915f5c46050707e045b4e56
NikoZBK <nikozbk@gmail.com>
2025-03-30 15:23:56 -0400

    create a card component and add tailwindcss

--- leafDex_Web/src/App.tsx
+++ leafDex_Web/src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import leafDex from './assets/LeafDex.png'
+import leafDex from './assets/LeafDex.png';
 
-import './App.css'
+import './App.css';
+import Card from './components/ui/Card';
 
 function App() {
   return (
-    <>{/* New top navigation */}
-           <nav className="top-nav">
+    <>
+      {/* New top navigation */}
+      <nav className="top-nav">
         <ul className="nav-list">
           <li>
-            <a href="#home" className="nav-link">Home</a>
+            <a href="#home" className="nav-link">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about" className="nav-link">About</a>
+            <a href="#about" className="nav-link">
+              About
+            </a>
           </li>
           <li>
-            <a href="#products" className="nav-link">Products</a>
+            <a href="#products" className="nav-link">
+              Products
+            </a>
           </li>
           <li>
-            <a href="#contact" className="nav-link">Contact</a>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
           </li>
-        </ul> </nav>
-{/* 
-    /*logo */ }
-      <div>
+        </ul>{' '}
+      </nav>
+      {/* 
+    /*logo */}
+      <Card>
         <img src={leafDex} className="logo" alt="Vite logo" />
-      </div>
+      </Card>
       <h1>Leafdex</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
