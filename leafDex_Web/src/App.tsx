0f20d538241bfa1fae66fe5a5e413800fb4dac27
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2025-03-30 14:24:57 -0400

    log in buttons implemented

--- leafDex_Web/src/App.tsx
+++ leafDex_Web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import leafDex from './assets/LeafDex.png'
 
 import './App.css'
@@ -6,21 +6,30 @@ import './App.css'
 function App() {
   return (
     <>{/* New top navigation */}
-           <nav className="top-nav">
-        <ul className="nav-list">
-          <li>
-            <a href="#home" className="nav-link">Home</a>
-          </li>
-          <li>
-            <a href="#about" className="nav-link">About</a>
-          </li>
-          <li>
-            <a href="#products" className="nav-link">Products</a>
-          </li>
-          <li>
-            <a href="#contact" className="nav-link">Contact</a>
-          </li>
-        </ul> </nav>
+      <nav className="top-nav">
+  <ul className="nav-list">
+    <li>
+      <a href="#home" className="nav-link">Home</a>
+    </li>
+    <li>
+      <a href="#about" className="nav-link">About</a>
+    </li>
+    <li>
+      <a href="#products" className="nav-link">Products</a>
+    </li>
+    <li>
+      <a href="#contact" className="nav-link">Contact</a>
+    </li>
+
+  <div className="log-in">
+    <button className="login-button" >
+      Log in
+    </button>
+    <button className="signup-button">
+      Sign up
+    </button>
+  </div>  </ul>
+</nav>
 {/* 
     /*logo */ }
       <div>
