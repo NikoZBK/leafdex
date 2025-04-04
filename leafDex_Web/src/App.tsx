924ea9b042368702c347afa133acf2d2a3d1dd30
NikoZBK <nikozbk@gmail.com>
2025-04-03 22:00:17 -0400

    refactor: remove App.css and update navigation to use DaisyUI #25
    - Deleted App.css to streamline styling and reduce unused CSS.
    - Replaced the previous top navigation with a new DaisyUI-based navigation structure in App.tsx.
    - Introduced shared classes for navigation links to enhance consistency and maintainability.

--- leafDex_Web/src/App.tsx
+++ leafDex_Web/src/App.tsx
@@ -1,36 +1,41 @@
 import Test from './components/Test';
 import { PlantProvider } from './context/PlantContext';
-
 import './index.css';
+
+// Shared classes for navigation
+const navLinkClasses =
+  'text-primary hover:bg-base-100 rounded-lg transition-colors duration-300';
 
 function App() {
   return (
     <>
-      {/* New top navigation */}
-      <nav className="top-nav">
-        <ul className="nav-list">
-          <li>
-            <a href="#home" className="nav-link">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="nav-link">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#products" className="nav-link">
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="nav-link">
-              Contact
-            </a>
-          </li>
-        </ul>{' '}
-      </nav>
+      {/* New top navigation using DaisyUI */}
+      <div className="navbar bg-base-200 fixed top-0 left-0 w-full z-50">
+        <div className="navbar-start">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a href="#home" className={navLinkClasses}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" className={navLinkClasses}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#products" className={navLinkClasses}>
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className={navLinkClasses}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <PlantProvider>
         <Test />
       </PlantProvider>
